be2ba303c3eecd1a2112c10a3cd48efff4a2aaf2
iMac <imac@iMac-de-iMac.local>
2021-06-22 20:46:07 -0500

    hi there

---
+++ config-service/src/main/java/ucuenca/ejemplo/store/config/ConfigServiceApplication.java
@@ -0,0 +1,15 @@
+package ucuenca.ejemplo.store.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@EnableConfigServer
+@SpringBootApplication
+public class ConfigServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigServiceApplication.class, args);
+	}
+
+}
